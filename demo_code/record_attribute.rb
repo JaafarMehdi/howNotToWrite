b8ed596dcb7c6424c1fab6c30c8e09be23926994
Mehdi Ben Jaafar <mehdi.ben.jaafar.tech@gmail.com>
2024-03-12 12:49:34 +0100

    initial case

---
+++ demo_code/record_attribute.rb
@@ -0,0 +1,17 @@
+module RecordAttribute
+
+  def list_kind_from_data(data)
+    first_matched_record = data.first[1].dig(:matched, :new_version)&.compact&.first
+    return if first_matched_record.blank?
+
+    first_matched_record.dig(:attributes, :kind)
+  end
+
+  def list_kind_from_upload(data)
+    result = list_kind_from_data(data)
+    # above method checks only matched data, if there is none we need to check unmatched
+    return result if result.present?
+
+    data.first[1].dig(:unmatched, 0, :attributes, :kind)
+  end
+end
