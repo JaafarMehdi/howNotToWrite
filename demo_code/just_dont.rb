d049f4f65684e2eda21d78fbdc2166818c30c888
Mehdi Ben Jaafar <mehdi.ben.jaafar.tech@gmail.com>
2024-03-12 13:52:13 +0100

    fixed the hell out of these puny examples

--- demo_code/just_dont.rb
+++ demo_code/just_dont.rb
@@ -7,20 +7,26 @@ module JustDont
     :quantity
   ].freeze
 
-  def check_all_heades_present(file_headers)
-    (ROW_HEADERS - file_headers).empty?
+  def check_all_heades_present(file_headers, expanded=false)
+    required_headers = ROW_HEADERS.dup
+    required_headers << :expanded_header if expanded
+    (required_headers - file_headers).empty?
   end
 
-  def get_active_user
+  def get_active_users_ids
     UserModel.where(active: true).pluck(:id)
   end
 
   def active_user?(user)
-    return 'active' if user.deactivation_date = nil
+    user.deactivation_date.blank?
+  end
+
+  def user_status(user)
+    return 'inactive' if user.deactivation_date.present?
+    'active'
   end
 
   def addition(a, b)
-    #  return the mathematical sum of a and b
     a + b
   end
 end
